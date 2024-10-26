219fe50787eedfcf63ac78ccb0493ebad646f30d
Karin <abramowitzkarin@gmail.com>
2024-10-26 10:45:49 +0300

    2nd commit 25/10/2024

--- app/src/main/java/com/example/sukkotproject/MainActivity.java
+++ app/src/main/java/com/example/sukkotproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sukkotproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +28,43 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        int i = 0;
+        String[] x = {"! Wow!", "!", "?", "?!", "...", "...?", " this", "? Sure!", "? No", "? Yes", "? Maybe"};
+        Random r = new Random();
         TextView txt = findViewById(R.id.textView);
+        txt.setText("");
         EditText hint = findViewById(R.id.Hint);
-        Button clr = findViewById(R.id.button2);
         Button changeTxt = findViewById(R.id.button);
+        Button clr = findViewById(R.id.button2);
+        clr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                while(i<3)
+                {
+                if(i == 0)
+                {
+                    txt.setTextColor(Color.RED);
+                }
+                else if(i == 1)
+                {
+                    txt.setTextColor(Color.BLUE);
+                }
+                else if(i == 2)
+                {
+                    txt.setTextColor(Color.BLACK);
+                }
+                }
+            }
+        });
+        changeTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String et = hint.getText().toString();
+                String x2 = x[r.nextInt(11)];
+                txt.setText(et + x2);
+            }
+        });
+
 
     }
 }
