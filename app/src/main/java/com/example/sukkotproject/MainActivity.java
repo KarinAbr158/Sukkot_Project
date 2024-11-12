a9c2750f7bd73679c56b468e44a6c0b334c8f5f7
Karin <abramowitzkarin@gmail.com>
2024-11-12 13:46:06 +0200

    5th commit 25/10/2024 added changes: Button clr changed to Button color c[r.nextInt(3)] changed to c[r.nextInt(c.length)] x[r.nextInt(11)] changed to x[r.nextInt(x.length)] 12/11/2024 deleted EdgeToEdge

--- app/src/main/java/com/example/sukkotproject/MainActivity.java
+++ app/src/main/java/com/example/sukkotproject/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
